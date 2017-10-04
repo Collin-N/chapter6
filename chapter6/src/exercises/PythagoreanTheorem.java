cca2123f4819dbd55429cd5bbdaeff5c7182114a
Collin N <bubba9n500@gmail.com>
2017-10-04 08:41:59 -0500

    update

--- chapter6/src/exercises/PythagoreanTheorem.java
+++ chapter6/src/exercises/PythagoreanTheorem.java
@@ -10,49 +10,68 @@ public class PythagoreanTheorem {
 		String stop = " ";
 		Scanner input = new Scanner(System.in);
 		String chooseSideString = null;
-		while(stop != "stop") {
+		double Area;
+		double Perimeter;
+		boolean condition = true;
+		while(condition) {
 			
 		
-		System.out.println("Trying to solve a right triangle? \nPlease enter which side you are solving for >> A\nB\nC");
+		System.out.println("Trying to solve a right triangle? \nPlease enter which side you are solving for >> \nA\nB\nC");
 		chooseSideString = input.nextLine();
-		if(chooseSideString.equalsIgnoreCase("A"));
+		
+		if(chooseSideString.equalsIgnoreCase("A"))
 		{
 			System.out.println("Please enter side B");
 			sideB = input.nextDouble();
 			System.out.println("please enter side C");
 			sideC = input.nextDouble();
-			sideA = Math.sqrt(sideC) - Math.sqrt(sideB);
+			sideA = (sideB * sideB) + (sideC * sideC);
 			
-			System.out.println("Side A is " + sideA);
-			
-			
+			sideA = Math.sqrt(sideA);
+			Area =  0.5 * sideB * sideC;
+			Perimeter = sideC + sideB + sideA;
+			System.out.println("Side c is " + sideC + " The perimeter is " + Perimeter + "the area is " + Area);
+		
 		}
-		if(chooseSideString.equalsIgnoreCase("B"));
+		if(chooseSideString.equalsIgnoreCase("B"))
 		{
 			System.out.println("Please enter side A");
-			sideB = input.nextDouble();
+			sideA = input.nextDouble();
 			System.out.println("please enter side C");
 			sideC = input.nextDouble();
-			sideA = Math.sqrt(sideA) - Math.sqrt(sideC);
+			sideB = (sideA * sideA) + (sideC * sideC);
 			
-			System.out.println("Side B is " + sideB);
+			sideB = Math.sqrt(sideB);
+			Area =  0.5 * sideA * sideC;
+			Perimeter = sideC + sideB + sideA;
+			System.out.println("Side b is " + sideB + " The perimeter is " + Perimeter + "the area is " + Area);
 			
 			
 		}
-		if(chooseSideString.equalsIgnoreCase("C"));
+		if(chooseSideString.equalsIgnoreCase("C"))
+		
 		{
 			System.out.println("Please enter side A");
-			sideB = input.nextDouble();
+			sideA = input.nextDouble();
 			System.out.println("please enter side B");
-			sideC = input.nextDouble();
-			sideA = Math.sqrt(sideA) - Math.sqrt(sideB);
+			sideB = input.nextDouble();
+			sideC = (sideA * sideA) + (sideB * sideB);
 			
-			System.out.println("Side C is " + sideC);
-			
+			sideC = Math.sqrt(sideC);
+			Area =  0.5 * sideA * sideB;
+			Perimeter = sideC + sideB + sideA;
+			System.out.println("Side c is " + sideC + " The perimeter is " + Perimeter + "the area is " + Area);
 			
 		  }
+		
+		
 			System.out.println("would you like to stop? input stop to stop, input no to continue > ");
 			stop = input.nextLine();
+			if("stop".equalsIgnoreCase(stop)) 
+				condition = false;
+			else
+				condition = true;
+			
 		}
 	}
 
